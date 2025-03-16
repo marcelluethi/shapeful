57dd77afa7f1ba11fc77b6debd9d3ae65e8a0e8f
Marcel Lüthi <marcel.luethi@inf.ethz.ch>
2025-03-16 16:04:02 +0100

    add more tests and

--- src/main/scala/shapeful/tensor/Shape.scala
+++ src/main/scala/shapeful/tensor/Shape.scala
@@ -6,6 +6,10 @@ import scala.Tuple.Concat
 
 import shapeful.tensor.TupleHelpers.*
 
+/**
+ * Represents the shape of a tensor. Dims is a tuple with a type for each dimension.
+ * Some operations assume that the types are unique, but this is not enforced.
+ */
 class Shape[Dims <: Tuple](dimTuple: ToIntTuple[Dims]):
     inline def dim[A] : Int =
         val i : Int = inlineIndexOf[Dims, A]
@@ -38,10 +42,19 @@ class Shape[Dims <: Tuple](dimTuple: ToIntTuple[Dims]):
         val newdims = Tuple.fromArray(newDimList.toArray).asInstanceOf[ToIntTuple[Remove[Dims, A]]]
         new Shape(newdims)
 
+    inline def rename[NewDims <: Tuple](using ev: ValueOf[Tuple.Size[NewDims]], ev2: ValueOf[Tuple.Size[Dims]]): Shape[NewDims] = {
+        inline if constValue[Tuple.Size[NewDims]] != constValue[Tuple.Size[Dims]] then
+            compiletime.error("New dimensions must have the same length as original dimensions")
+        new Shape[NewDims](dimTuple.asInstanceOf[ToIntTuple[NewDims]])
+}
 
 object Shape:
     def empty = new Shape[EmptyTuple.type](EmptyTuple) 
     def apply[A](n : Int) = new Shape[Tuple1[A]](Tuple1(n)) 
     def apply[A, B](m : Int, n : Int) = new Shape[(A, B)]((m, n))
+    def apply[A, B, C](m : Int, n : Int, o : Int) = new Shape[(A, B, C)]((m, n, o))
+    def apply[A, B, C, D](m : Int, n : Int, o : Int, p : Int) = new Shape[(A, B, C, D)]((m, n, o, p))
+    def apply[A, B, C, D, E](m : Int, n : Int, o : Int, p : Int, q : Int) = new Shape[(A, B, C, D, E)]((m, n, o, p, q))
+    
     
 

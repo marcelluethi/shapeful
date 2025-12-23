f477cd94e33e177e506bf898c406f52166d9454b
Marcel Luethi <marcel.luethi@inf.ethz.ch>
2025-12-23 10:02:31 +0100

    putting nn modules back in with Value type parameter

--- nn/src/main/scala/nn/LinearLayer.scala
+++ nn/src/main/scala/nn/LinearLayer.scala
@@ -1,12 +1,12 @@
 package nn
 
 import shapeful.*
-import shapeful.Conversions.given
+import shapeful.random.Random
 import shapeful.random.Random.Key
 
 object LinearLayer:
 
-    case class Params[In, Out](weight: Tensor2[In, Out], bias: Tensor1[Out])
+    case class Params[In, Out](weight: Tensor2[In, Out, DType.Float32.type], bias: Tensor1[Out, DType.Float32.type])
 
     object Params:
         given [I : Label, O : Label]: TensorTree[Params[I, O]] = TensorTree.derived
@@ -15,30 +15,36 @@ object LinearLayer:
         def apply[In : Label, Out : Label](paramKey: Key)(
             inputDim: Dim[In],
             outputDim: Dim[Out],
-        ): Params[In, Out] = Params(
-            weight = Tensor.randn(Shape(inputDim, outputDim), paramKey),
-            bias = Tensor.zeros(Shape(outputDim)),
-        )
+        ): Params[In, Out] = 
+            val mean = Tensor0.of[DType.Float32.type].apply(0f)
+            val std = Tensor0.of[DType.Float32.type].apply(1f)
+            Params(
+                weight = Random.normal(paramKey, Shape(inputDim, outputDim), mean, std),
+                bias = Tensor.of[DType.Float32.type].zeros(Shape(outputDim)),
+            )
 
-case class LinearLayer[In : Label,Out : Label](params: LinearLayer.Params[In, Out]) extends Function[Tensor1[In], Tensor1[Out]]:
-    override def apply(x: Tensor1[In]): Tensor1[Out] =
+case class LinearLayer[In : Label,Out : Label](params: LinearLayer.Params[In, Out]) extends Function[Tensor1[In, DType.Float32.type], Tensor1[Out, DType.Float32.type]]:
+    override def apply(x: Tensor1[In, DType.Float32.type]): Tensor1[Out, DType.Float32.type] =
         import params.{weight, bias}
         x.contract(Axis[In])(weight) + bias
 
 object LinearMap:
 
-    case class Params[In](weight: Tensor1[In], bias: Tensor0)
+    case class Params[In](weight: Tensor1[In, DType.Float32.type], bias: Tensor0[DType.Float32.type])
 
     object Params:
         given [In : Label]: TensorTree[Params[In]] = TensorTree.derived
         given [In : Label]: ToPyTree[Params[In]] = ToPyTree.derived
 
-        def apply[In : Label](paramKey: Key)(inputDim: Dim[In]): Params[In] = Params(
-            weight = Tensor.randn(Shape(inputDim), paramKey),
-            bias = Tensor0(0.0f),
-        )
+        def apply[In : Label](paramKey: Key)(inputDim: Dim[In]): Params[In] = 
+            val mean = Tensor0.of[DType.Float32.type].apply(0f)
+            val std = Tensor0.of[DType.Float32.type].apply(1f)
+            Params(
+                weight = Random.normal(paramKey, Shape(inputDim), mean, std),
+                bias = Tensor0.of[DType.Float32.type].apply(0.0f),
+            )
 
-case class LinearMap[In : Label](params: LinearMap.Params[In]) extends Function[Tensor1[In], Tensor0]:
-    override def apply(x: Tensor1[In]): Tensor0 = 
+case class LinearMap[In : Label](params: LinearMap.Params[In]) extends Function[Tensor1[In, DType.Float32.type], Tensor0[DType.Float32.type]]:
+    override def apply(x: Tensor1[In, DType.Float32.type]): Tensor0[DType.Float32.type] = 
         import params.{weight, bias}
         x.contract(Axis[In])(weight) + bias
